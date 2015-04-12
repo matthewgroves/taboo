f45577e87c93a14bcbb0d452f58668bc37270407
glspisso <glspisso@gmail.com>
2015-04-12 02:23:09 +0100

    Taboo project main function

---
+++ taboo.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+/* ======================================= *
+ * Taboo Project: our goal is to create an 
+ * agent based / genetic algorithm model to 
+ * study the emergence and persistence of taboos
+ * in societies.
+ * This is mostly for fun.
+ * ======================================= */
+
+/*
+ * Agents should : consume "goods" which are randomly available;
+ * 				   "goods" might be dangerous (yield some chance of killing the agent) and yield utility
+ * 					have some internal representation  of whether they enjoy the goods
+ * 
+ * Policies: 		have different modular components: 
+ * 					"what" | Some good which is sanctioned 
+ * 					"how"  | How likely it is for the system to punish (legal system efficiency)
+ * 			  "punishment" | [0,1] punishment intensity 
+ 
+ *			This is just a sketch, what else is relevant here?
+ * 			Main goals: how do taboos emerge? Can they be removed overtime?
+ * 						also, practice genetic/abm models.
+ *  */
+
+int main(){
+	
+	"Hello, world! I am an agent and I like eating sacred pentolotti for dinner. Pleease, no hurt, no kill, keep alive and next time I shall bring to you"
+	
+	return 0;
+	
+	};
